73d609d1ec3d32ded736b21bf048414755c46e8e
mangege <mr.mangege@gmail.com>
2012-10-21 13:02:53 +0800

    firest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inav::Application.config.secret_token = '1742f1f0f1d50cc6435692cde6e344e862582f16f31d3b94a7e6235772add2313a319f70d4684b1535135275d23027f88ab4128e9d09d76b9013364156325fe2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Inav::Application.config.session_store :cookie_store, key: '_inav_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Inav::Application.config.session_store :active_record_store
