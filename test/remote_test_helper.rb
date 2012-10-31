8f485622f063fa0b0d6500e6d5ba74a239eae0dc
mangege <mr.mangege@gmail.com>
2012-10-31 23:51:36 +0800

    测试会重置实例变量,导致每次都读文件

--- test/remote_test_helper.rb
+++ test/remote_test_helper.rb
@@ -2,13 +2,10 @@
 require 'test_helper'
 
 WebMock.disable!
+system("bundle exec rails r -e development #{Rails.root}/lib/tasks/remote_testing.rb")
 
 class ActiveSupport::TestCase
   def default_remote_user_attrs
-    if @default_remote_user_attrs.nil?
-      system('bundle exec rails r lib/tasks/remote_testing.rb')
-      @default_remote_user_attrs = MultiJson.load( File.read("#{Rails.root}/tmp/default_test_user.json") )
-    end
-    @default_remote_user_attrs
+      @default_remote_user_attrs ||= MultiJson.load( File.read("#{Rails.root}/tmp/default_test_user.json") )
   end
 end
