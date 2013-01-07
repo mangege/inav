a0df3f0868a05c703fbdfe6db4ac82648830e165
mangege <mr.mangege@gmail.com>
2013-01-07 22:20:56 +0800

    HTML5 Shiv to local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Inav::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-   config.assets.precompile += %w( ace.js )
+   config.assets.precompile += %w( ace.js html5.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
